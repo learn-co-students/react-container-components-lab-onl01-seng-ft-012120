5408a80108a0ab230f25766bbd1b46ca383ad65a
github username <github email address>
2020-05-22 10:01:24 -0400

    Done.

--- src/components/MovieReviews.js
+++ src/components/MovieReviews.js
@@ -1,1 +1,23 @@
 // Code MovieReviews Here
+import React from 'react'
+
+const MovieReviews = (props) => {
+
+    const reviews = props.reviews.map(review => {
+        return(
+            <li className="review">
+                <h3>{review.display_title}</h3>
+                <h4>{review.headline} </h4>
+                <a href={review.link.url}>{review.link.suggested_link_text}</a>
+            </li>
+        )
+    })
+
+    return (
+        <div className="review-list">
+            { reviews }
+        </div>
+    )
+}
+
+export default MovieReviews
