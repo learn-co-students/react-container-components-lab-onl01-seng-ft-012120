da89be6e233721d65f9748ae6bdde2f2490e11eb
andrew041994 <andrew041994@gmail.com>
2020-05-31 19:03:41 -0400

    Done.

--- src/components/MovieReviews.js
+++ src/components/MovieReviews.js
@@ -1,1 +1,8 @@
-// Code MovieReviews Here
+import React from 'react';
+
+const MovieReviews = props => <div className="review-list" > <ul>
+                                    {props.movies.map(movie => <li key={movie.headline}>{movie.display_title}</li>)}
+                                </ul>
+                            </div>
+
+export default MovieReviews
--- src/components/SearchableMovieReviewsContainer.js
+++ src/components/SearchableMovieReviewsContainer.js
@@ -4,6 +4,49 @@ import MovieReviews from './MovieReviews'
 
 const NYT_API_KEY = 'dGpQ5OmGP2SgfvZimlpCUoF4iOag9qzZ';
 const URL = 'https://api.nytimes.com/svc/movies/v2/reviews/search.json?'
-            + `api-key=${NYT_API_KEY}`;
+            + `api-key=${NYT_API_KEY}&query=`;
 
 // Code SearchableMovieReviewsContainer Here
+
+export default class SearchableMovieReviewsContainer extends Component {
+
+    constructor() {
+        super() 
+        this.state = {
+            query: "",
+            movies: []
+        }
+    }
+
+    // async fetchMovies(event) {
+    //     // event.preventDefault()
+
+    //     let resp = await 
+    //     fetch(`https://api.nytimes.com/svc/movies/v2/reviews/search.json?api-key=dGpQ5OmGP2SgfvZimlpCUoF4iOag9qzZ&query=${this.state.query}`)
+    //     let resp1 = await resp.json()
+    //     let movies = resp1.results
+    //     // console.log(movies)
+    //     this.setState({movies: movies})
+
+
+    // }
+
+    fetchMovies = (event) => {
+        event.preventDefault();
+    
+        fetch(URL.concat(this.state.query))
+          .then((res) => res.json())
+          .then((res) => this.setState({ movies: res.results }));
+      };
+
+    render() {
+        return (
+            <div className="searchable-movie-reviews">
+                <form onSubmit={this.fetchMovies} >
+                <input type="text" value={this.state.query} onChange={event => this.setState({query: event.target.value})} />
+                </form>
+              <MovieReviews movies={this.state.movies}/> 
+            </div>
+        )
+    }
+}
